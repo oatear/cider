ff264633f27b6db8374b157a357866dac2a13a6d
Hristo Iankov <35353708+hristoiankov@users.noreply.github.com>
2022-02-08 20:41:16 -0500

    Template styling

--- cider-app/src/app/card-templates/card-templates.component.ts
+++ cider-app/src/app/card-templates/card-templates.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SecurityContext } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
@@ -9,8 +9,8 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class CardTemplatesComponent implements OnInit {
   htmlEditorOptions: any = {theme: 'vs-dark', language: 'html', automaticLayout: true};
   cssEditorOptions: any = {theme: 'vs-dark', language: 'css', automaticLayout: true};
-  html: string = '<h2>Poison Apple</h2>\n<div>Activate this card now.</div>';
-  css: string = 'padding: 25px;';
+  html: string = '<div>\n\t<h2>Poison Apple</h2>\n\t<p>Activate this card now.</p>\n</div>';
+  css: string = 'div {\n\tpadding: 25px;\n}\nh2 {\n\tcolor: rgb(129, 156, 89);\n}';
 
   constructor(private domSanitizer: DomSanitizer) { }
 
@@ -19,6 +19,13 @@ export class CardTemplatesComponent implements OnInit {
 
   safeCss(style: string) {
     return this.domSanitizer.bypassSecurityTrustStyle(style);
+  }
+
+  safeHtmlAndStyle(html: string, css: string) {
+    let sanitizedStyle = css.replace(/([^{}]*\{)/g, '.card-preview $1');
+    let safeStyle = this.domSanitizer.sanitize(SecurityContext.STYLE, sanitizedStyle);
+    let safeHtml = this.domSanitizer.sanitize(SecurityContext.HTML, html);
+    return this.domSanitizer.bypassSecurityTrustHtml(`${safeHtml}<style>${safeStyle}</style>`);
   }
 
 }
