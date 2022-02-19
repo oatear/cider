9eb5c2f24531a58d1c2f3e1517358acab778df5d
Hristo Iankov <35353708+hristoiankov@users.noreply.github.com>
2022-02-19 10:56:06 -0500

    Files saved as ArrayBuffer, import/export now supports files

--- cider-app/src/app/data-services/services/assets.service.ts
+++ cider-app/src/app/data-services/services/assets.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@angular/core';
+import { Injectable, PLATFORM_INITIALIZER } from '@angular/core';
 import { Asset } from '../types/asset.type';
 import { AppDB } from '../indexed-db/db';
 import { FieldType } from '../types/field-type.type';
 import { GamesChildService } from '../indexed-db/games-child.service';
 import { GamesService } from './games.service';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { SearchParameters } from '../types/search-parameters.type';
 
 @Injectable({
   providedIn: 'root'
@@ -54,5 +55,60 @@ export class AssetsService extends GamesChildService<Asset, number> {
       let filteredAssets = assets.filter(asset => asset.name && asset.name.replace(/ /g, '').toLowerCase() === name);
       return filteredAssets && filteredAssets.length > 0 ? filteredAssets[0] : {} as Asset;
     });
+  }
+
+  private static arrayBufferToBlob(buffer: ArrayBuffer, type: string): Blob {
+    return new Blob([buffer], {type: type});
+  }
+
+  /**
+   * Insert an array buffer and type into the asset
+   * @param entity 
+   */
+  private static insertArrayBuffer(entity: Asset): Promise<Asset> {
+    return entity.file.arrayBuffer().then(buffer => {
+      (<any>entity).buffer = buffer;
+      (<any>entity).type = entity.file.type;
+      (<any>entity).file = undefined;
+      return entity;
+    });
+  }
+
+  /**
+   * Insert a file/blob into the asset
+   * @param entity 
+   */
+  private static insertFile(entity: Asset): Asset {
+      if (!(<any>entity).buffer || !(<any>entity).type) {
+        return entity;
+      }
+    const blob: Blob = AssetsService.arrayBufferToBlob((<any>entity).buffer, (<any>entity).type);
+    entity.file = new File([blob], entity.name);
+    (<any>entity).buffer = undefined;
+    (<any>entity).type = undefined;
+    return entity;
+  }
+
+  override search(searchParameters: SearchParameters) {
+    return super.search(searchParameters).then(result => {
+      result.records = result.records.map(AssetsService.insertFile);
+      return result;
+    });
+  }
+
+  override get(id: number) {
+    return super.get(id).then(AssetsService.insertFile);
+  }
+
+  override getAll() {
+    return super.getAll().then(entities => entities.map(AssetsService.insertFile));
+  }
+
+  override create(entity: Asset) {
+    return AssetsService.insertArrayBuffer(entity).then(entity => super.create(entity));
+  }
+
+  override update(id: number, entity: Asset) {
+    return AssetsService.insertArrayBuffer(entity).then(entity => super.update(id, entity));
   }
 }
