96ebd89d7a834007ab8e8ed4e6f30fdd848e20dc
Hristo Iankov <35353708+hristoiankov@users.noreply.github.com>
2022-02-21 18:14:29 -0500

    cleanup

--- node_modules/dexie-export-import/dist/tson-arraybuffer.d.ts
+++
@@ -1,8 +0,0 @@
-declare const _default: {
-    arraybuffer: {
-        test(x: any): boolean;
-        replace(b: any): any;
-        revive(b64: any): any;
-    };
-};
-export default _default;
--- node_modules/dexie-export-import/dist/tson-typed-array.d.ts
+++
@@ -1,2 +0,0 @@
-declare const exportObj: {};
-export default exportObj;
