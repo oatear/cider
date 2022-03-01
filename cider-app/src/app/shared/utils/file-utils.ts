6584347cf83635e4ef6bec19956812b78fc84888
Hristo Iankov <35353708+hristoiankov@users.noreply.github.com>
2022-03-01 17:09:27 -0500

    Update file-utils.ts

--- cider-app/src/app/shared/utils/file-utils.ts
+++ cider-app/src/app/shared/utils/file-utils.ts
@@ -5,12 +5,11 @@ export default class FileUtils {
     static saveAs(data: Blob, name: string): void {
         if (navigator.userAgent.match(/CriOS/i)) {
             // Chrome iOS download procedure
-            const blob = new Blob([data], {type: 'octet/stream'});
             const reader = new FileReader();
             reader.onload = function() {
                 window.location.href = <string>reader.result;
             }
-            reader.readAsDataURL(blob);
+            reader.readAsDataURL(data);
         } else {
             FileSaver.saveAs(data, name);
         }
