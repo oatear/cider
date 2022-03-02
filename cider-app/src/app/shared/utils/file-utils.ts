179b6428315eacc8c1208dcccec2085cb7a4d4e6
Hristo Iankov <35353708+hristoiankov@users.noreply.github.com>
2022-03-02 17:27:19 -0500

    iOS fix attempt latest

--- cider-app/src/app/shared/utils/file-utils.ts
+++ cider-app/src/app/shared/utils/file-utils.ts
@@ -2,16 +2,40 @@ import * as FileSaver from "file-saver";
 
 export default class FileUtils {
 
+    /**
+     * Save file with the given name
+     * 
+     * @param data 
+     * @param name 
+     */
     static saveAs(data: Blob, name: string): void {
         if (navigator.userAgent.match(/CriOS/i)) {
             // Chrome iOS download procedure
             const reader = new FileReader();
             reader.onload = function() {
-                window.location.href = <string>reader.result;
+                //window.location.href = <string>reader.result;
+                FileUtils.anchorUrlDownload(<string>reader.result);
             }
             reader.readAsDataURL(data);
         } else {
             FileSaver.saveAs(data, name);
         }
+    }
+
+    /**
+     * Download file using a temporary anchor tag
+     * placed onto the dom
+     * 
+     * @param url 
+     */
+    private static anchorUrlDownload(url: string) {
+        const a = document.createElement("a");
+      document.body.appendChild(a);
+      a.setAttribute('style', 'display: none');
+      a.href = url;
+      a.download = "download";
+      a.click();
+      //window.URL.revokeObjectURL(url);
+      a.remove();
     }
 }
