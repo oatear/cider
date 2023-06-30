73181b44d72ede9a923ca2949c48e9b133811a97
Hristo Iankov <35353708+hristoiankov@users.noreply.github.com>
2023-06-29 21:10:47 -0400

    cached dropdown options on tables

---
+++ cider-app/src/app/data-services/pipes/entity-list-cached.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { EntityService } from '../types/entity-service.type';
+
+@Pipe({
+  name: 'entityListCached'
+})
+export class EntityListCachedPipe implements PipeTransform {
+
+  async transform(service: EntityService<any, any>, cache: Map<EntityService<any, any>, any[]>): Promise<any[] | undefined> {
+    if (!cache) {
+      return service.getAll();
+    }
+
+    const options = cache.get(service);
+    if (!options && service) {
+      const takenOptions = await service.getAll();
+      cache.set(service, takenOptions);
+      console.log('setOptionsCache', takenOptions);
+      return takenOptions;
+    }
+    return options;
+  }
+
+}
