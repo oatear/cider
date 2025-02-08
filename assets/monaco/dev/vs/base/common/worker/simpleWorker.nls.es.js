57a3df1dac086c28fad196ca20cd59868cdb8a1b
hristoiankov <35353708+hristoiankov@users.noreply.github.com>
2025-02-08 19:47:51 +0000

    Deploying to gh-pages from @ oatear/cider@6e2ecd4b0c782e1434331e330f3aea3e11b34524 🚀

---
+++ assets/monaco/dev/vs/base/common/worker/simpleWorker.nls.es.js
@@ -0,0 +1,41 @@
+/*!-----------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
+ * Released under the MIT license
+ * https://github.com/microsoft/vscode/blob/main/LICENSE.txt
+ *-----------------------------------------------------------*/
+
+define("vs/base/common/worker/simpleWorker.nls.es", {
+	"vs/base/common/platform": [
+		"_",
+	],
+	"vs/editor/common/languages": [
+		"matriz",
+		"booleano",
+		"clase",
+		"constante",
+		"constructor",
+		"enumeración",
+		"miembro de la enumeración",
+		"evento",
+		"campo",
+		"archivo",
+		"función",
+		"interfaz",
+		"clave",
+		"método",
+		"módulo",
+		"espacio de nombres",
+		"NULL",
+		"número",
+		"objeto",
+		"operador",
+		"paquete",
+		"propiedad",
+		"cadena",
+		"estructura",
+		"parámetro de tipo",
+		"variable",
+		"{0} ({1})",
+	]
+});
