920e58ffdb412827dcac33e80951858b1a78b7de
Hristo Iankov <35353708+hristoiankov@users.noreply.github.com>
2025-06-10 08:49:07 -0400

    card templates style fixes

---
+++ cider-app/src/app/settings-dialog/settings-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-settings-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './settings-dialog.component.html',
+  styleUrl: './settings-dialog.component.scss'
+})
+export class SettingsDialogComponent {
+
+}
